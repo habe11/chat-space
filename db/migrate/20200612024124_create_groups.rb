68e592fdb58fc7a04e24f40621e6459b09a38460
habe11 <aced119@gmail.com>
2020-06-12 18:26:35 +0900

    chat-space-group

---
+++ db/migrate/20200612024124_create_groups.rb
@@ -0,0 +1,11 @@
+class CreateGroups < ActiveRecord::Migration[6.0]
+  def change
+    create_table :groups do |t|
+      t.string :name, null: false
+      # NOT NULL制約
+      t.index :name, unique: true
+      # 一意性制約
+      t.timestamps
+    end
+  end
+end
